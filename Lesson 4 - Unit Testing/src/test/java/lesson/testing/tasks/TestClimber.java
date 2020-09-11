b2fecce7e450825c36dfa26cbdc9f9273519bee2
Sean Zammit <smez12345@gmail.com>
2020-09-11 16:48:25 +1000

    Added lesson 4

---
+++ Lesson 4 - Unit Testing/src/test/java/lesson/testing/tasks/TestClimber.java
@@ -0,0 +1,10 @@
+package lesson.testing.tasks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestClimber {
+    
+}
